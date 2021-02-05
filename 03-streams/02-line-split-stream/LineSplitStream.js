77c88f81afbb7ce2f141dec2a5bb7ff680ecbb4b
bonauze <bonauze@gmail.com>
2021-02-05 12:46:57 +0300

    added LineSplitStream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,34 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this._data = '';
   }
 
   _transform(chunk, encoding, callback) {
+    this._data += chunk.toString();
+    const lines = this._data.split(os.EOL);
+
+    if (lines.length === 1) {
+      callback();
+      return;
+    }
+
+    const lastLinesIndex = lines.length - 1;
+
+    lines.forEach((line, index) => {
+      if (index !== lastLinesIndex) {
+        this.push(line);
+      } else {
+        this._data = line;
+      }
+    });
+
+    callback();
   }
 
   _flush(callback) {
+    this._data.split(os.EOL).forEach(this.push.bind(this));
+    callback();
   }
 }
 
